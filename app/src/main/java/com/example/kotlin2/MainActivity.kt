02f7ead70a1932e8fc34594bbaf614942ec48bd9
Denis <den.potemkin2015@mail.ru>
2024-09-13 01:30:52 +0300

    +kotlin3

--- app/src/main/java/com/example/kotlin2/MainActivity.kt
+++ app/src/main/java/com/example/kotlin2/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlin2
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,6 +12,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var viewModel: MyViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
--- app/src/main/java/com/example/kotlin2/fragment1.kt
+++ app/src/main/java/com/example/kotlin2/fragment1.kt
@@ -1,16 +1,18 @@
 package com.example.kotlin2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.kotlin2.databinding.FragmentFragment1Binding
 
 class fragment1 : Fragment() {
     private lateinit var binding: FragmentFragment1Binding
+    private lateinit var dataViewModel: MyViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +22,16 @@ class fragment1 : Fragment() {
         binding.btnNext.setOnClickListener {
             findNavController().navigate(R.id.
             action_fragment1_to_fragment2)
+        }
+
+        val dataRemoteDataSource = DataRemoteDataSourceImpl()
+        val dataRepository = DataRepository(dataRemoteDataSource)
+        val getDataUseCase = GetDataUseCase(dataRepository)
+
+        dataViewModel = ViewModelProvider(this, MyViewModelFactory(getDataUseCase))[MyViewModel::class.java]
+
+        dataViewModel.data.observe(viewLifecycleOwner) { data ->
+            binding.datatext.text = data
         }
         return binding.root
     }
